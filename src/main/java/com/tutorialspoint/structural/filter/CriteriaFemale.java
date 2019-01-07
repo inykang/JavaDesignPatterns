17f57d2b01bdcef191e2faeb82c408f7c1c74e52
inykang <iny.kang@gmail.com>
2019-01-07 18:13:41 +0900

    update bridge, filter patterns

---
+++ src/main/java/com/tutorialspoint/structural/filter/CriteriaFemale.java
@@ -0,0 +1,18 @@
+package com.tutorialspoint.structural.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CriteriaFemale implements Criteria {
+    @Override
+    public List<Person> meetCriteria(List<Person> persons) {
+        List<Person> femalePerson = new ArrayList<Person>();
+
+        for (Person person : persons) {
+            if(person.getGender().equalsIgnoreCase("FEMALE")){
+                femalePerson.add(person);
+            }
+        }
+        return femalePerson;
+    }
+}
